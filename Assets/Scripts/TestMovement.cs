91a2e0bbfff2fa6b00af4ee0808d34110813c3ab
Sam Rowlands <srowlands@hotmail.co.uk>
2016-02-13 12:09:41 +0000

    Initial project & playing with networking.

---
+++ Assets/Scripts/TestMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestMovement: NetworkBehaviour
+{
+    public void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        float x = Input.GetAxis("Horizontal") * 0.1f;
+        float z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
